998393568d7e2ed732017aae07d45a40c02852b8
Mohammed Rehan Alam <multilynkqr@gmail.com>
2026-06-13 11:19:18 +0530

    updated the config file

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,6 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/11.2.0/firebase
 
 const firebaseConfig = {
     apiKey: "AIzaSyCfbaLbcx8D7QguQONhL6iDiaAvgQbGhVA",
-    authDomain: "multilynk-qr.firebaseapp.com",
     projectId: "multilynk-qr",
     appId: "1:329033716655:web:adb0399032ae48e230955a"
 };
